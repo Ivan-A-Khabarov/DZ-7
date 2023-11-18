397f54a66e5940d3d34cfc91d5fdb3ab0f84b63d
IvanKhabarov <ivan@ihabarov.ru>
2023-11-18 15:19:24 +0300

    Решение к задаче 1

---
+++ DZ-7.1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+*/
+
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+SumMandN(M, N);
+void SumMandN(int M, int N)
+
+{
+    Console.WriteLine(SumMN(M - 1, N));
+}
+
+int SumMN(int M, int N)
+{
+    int result = M;
+    if (M == N)
+    return 0;
+    else
+    {
+        M++;
+        result = M + SumMN(M,N);
+        return result;
+    }
+}
