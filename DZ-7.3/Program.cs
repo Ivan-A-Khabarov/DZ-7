091e078295b270b510c0ff16f16df5b61ee82438
IvanKhabarov <ivan@ihabarov.ru>
2023-11-18 15:48:39 +0300

    Решение задачи 3

---
+++ DZ-7.3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+*/
+
+int[] array = [1, 2, 3, 4, 5];
+PrintArrayElements(array, array.Length - 1);
+
+void PrintArrayElements(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(array[index] + " ");
+        PrintArrayElements(array, index - 1);
+    }
+}
+Console.WriteLine();
