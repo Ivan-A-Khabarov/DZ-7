dfa5f131a32acaaf7fe0a1faaf0255129afc2dcf
Ivan-A-Khabarov <147238144+Ivan-A-Khabarov@users.noreply.github.com>
2023-11-25 12:46:29 +0300

    Update Program.cs

--- DZ-7.3/Program.cs
+++ DZ-7.3/Program.cs
@@ -2,7 +2,7 @@
 Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
 */
 
-int[] array = [1, 2, 3, 4, 5];
+int[] array = {1, 2, 3, 4, 5};
 PrintArrayElements(array, array.Length - 1);
 
 void PrintArrayElements(int[] array, int index)
